4c1ae37764f667d0f767d11cad6b62b403f8147d
Etienne Brouillard <etienne.brouillard@outlook.com>
2020-03-17 11:29:28 -0400

    fixed as per go module recommendation

--- demos/sdk/main.go
+++ demos/sdk/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"encoding/json"
+
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
@@ -14,23 +18,36 @@ func exitErrorf(msg string, args ...interface{}) {
 	os.Exit(1)
 }
 
+// The Lshortfile flag includes file name and line number in log messages.
+func init() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile)
+}
+
 func main() {
-	fmt.Printf("starting demo 1\n")
+	log.Printf("starting demo 1\n")
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("ca-central-1")},
 	)
-	
-	if err != nil{
+
+	sess.Handlers.Send.PushFront(func(r *request.Request) {
+		rParamsBytes, err := json.Marshal(r.Params)
+		if err != nil {}
+		// Log every request made and its payload
+		log.Printf("Request: %s/%s, Params: %s",
+			r.ClientInfo.ServiceName, r.Operation.Name, string(rParamsBytes))
+	})
+
+	if err != nil {
 		exitErrorf("Unable to create session, %v", err)
 	}
 	_, err = sess.Config.Credentials.Get()
 
-	if err != nil{
+	if err != nil {
 		exitErrorf("Credentials are invalid, %v", err)
 	}
 	svc := s3.New(sess)
 	result, err := svc.ListBuckets(nil)
-	
+
 	fmt.Println("Buckets:")
 	for _, b := range result.Buckets {
 		fmt.Printf("* %s created on %s\n",
